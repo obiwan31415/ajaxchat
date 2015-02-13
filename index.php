90dd538b5792dc7207bc2b0351107c1ccbba8716
obiwan31415 <obiwan314@gmail.com>
2015-02-13 14:46:50 +0100

    v0.1

--- index.php
+++ index.php
@@ -5,15 +5,64 @@
     <meta charset=utf-8>
     <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 </head>
+
+<?php
+session_start();
+ 
+function loginForm(){
+    echo'
+    <div id="loginform">
+    <form action="index.php" method="post">
+        <p>Please enter your name to continue:</p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+    </form>
+    </div>
+    ';
+}
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+?>
+
 <body>
-    <div id="wrapper">
+
+<?php
+if(!isset($_SESSION['name'])){
+    loginForm();
+}
+else{
+?>
+<div id="wrapper">
     <div id="menu">
-        <p class="welcome">Welcome, <b></b></p>
+        <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
         <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
         <div style="clear:both"></div>
     </div>
-     
-    <div id="chatbox"></div>
+ 
+    <div id="chatbox">
+        <?php
+
+        if(file_exists("log.html") && filesize("log.html") > 0){
+            $handle = fopen ("log.html", "w+");
+            fclose($handle);
+            $handle = fopen("log.html", "r");
+            $contents = fread($handle, filesize("log.html"));
+            fclose($handle);
+             
+            echo $contents;
+        } else {
+            echo "file log.html not present";
+        }
+        ?>
+    </div>
      
     <form name="message" action="">
         <input name="usermsg" type="text" id="usermsg" size="63" />
@@ -24,8 +73,51 @@
 <script type="text/javascript">
 // jQuery Document
 $(document).ready(function(){
- 
+    //If user wants to end session
+    $("#exit").click(function(){
+        var exit = confirm("Are you sure you want to end the session?");
+        if(exit==true){window.location = 'index.php?logout=true';}      
+    });  
+    //If user submits the form
+    $("#submitmsg").click(function(){   
+        var clientmsg = $("#usermsg").val();
+        $.post("post.php", {text: clientmsg});              
+        $("#usermsg").attr("value", "");
+        return false;
+    });
+    //Load the file containing the chat log
+    function loadLog(){     
+        var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+        $.ajax({
+            url: "log.html",
+            cache: false,
+            success: function(html){        
+                $("#chatbox").html(html); //Insert chat log into the #chatbox div   
+                
+                //Auto-scroll           
+                var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+                if(newscrollHeight > oldscrollHeight){
+                    $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+                }               
+            },
+        });
+    }
+
+    setInterval (loadLog, 2500);    //Reload file every 2500 ms or x ms 
 });
 </script>
+
+<?php
+}
+if(isset($_GET['logout'])){ 
+    //Simple exit message
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+    fclose($fp);
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
+?>
+
 </body>
 </html>
